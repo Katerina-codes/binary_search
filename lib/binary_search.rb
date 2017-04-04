23fa95afe1045ba92a61f68acdc68c15fe669ce3
katerina georgiou <katerinaldg@gmail.com>
2017-04-04 16:50:34 +0100

    Fixed indentation.

--- lib/binary_search.rb
+++ lib/binary_search.rb
@@ -5,22 +5,22 @@ class BinarySearch
   def find_element(array, element)
     low = 0
     high = array.length
-
+    
     average = ((high + low) / 2)
 
     if element == array[average]
-       return array[average]
+      return array[average]
     end
 
     until element == array[average]
       average = ((high + low) / 2)
-        if array[average] > element
-          high = average - 1
-        else
-          array[average] < element
-          low = average + 1
-        end
+      if array[average] > element
+        high = average - 1
+      else
+        array[average] < element
+        low = average + 1
       end
+    end
     array[average]
   end
 
